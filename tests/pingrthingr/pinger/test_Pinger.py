8f68f2441ebf078dcf992870dfec7d09433a217d
Adam Schumacher <adam@genericor.com>
2026-04-05 16:09:05 -0400

    formatting

--- tests/pingrthingr/pinger/test_Pinger.py
+++ tests/pingrthingr/pinger/test_Pinger.py
@@ -11,14 +11,15 @@ from pingrthingr.pinger import Pinger
 
 base_path = Path(__file__).parent
 
+
 @pytest.fixture
 def ping_response():
-    def _ping_response(testcase: str) -> Tuple[List[Host], Tuple[int,int]]:
+    def _ping_response(testcase: str) -> Tuple[List[Host], Tuple[int, int]]:
         results = []
         try:
             with open(f"{base_path}/resources/responses/{testcase}.json", "r") as f:
                 data = load(f)
-                for d in data['ping_response']:
+                for d in data["ping_response"]:
                     results.append(Host(**d))
         except FileNotFoundError:
             pytest.fail(
@@ -27,16 +28,23 @@ def ping_response():
         except (JSONDecodeError, TypeError) as e:
             pytest.fail(f"Error parsing test case {testcase}: {e}")
 
-        return results,data['callback_response'] 
+        return results, data["callback_response"]
 
     return _ping_response
 
+
 @pytest.mark.asyncio
-@pytest.mark.parametrize("test_case", ["all_success","all_failures", "one_failure"])
+@pytest.mark.parametrize("test_case", ["all_success", "all_failures", "one_failure"])
 async def test_Pinger(mocker, ping_response, test_case):
     ping_reponse_value, callback_response = ping_response(test_case)
-    mocker.patch("pingrthingr.pinger.pinger.async_multiping", return_value=ping_reponse_value)
+    mocker.patch(
+        "pingrthingr.pinger.pinger.async_multiping", return_value=ping_reponse_value
+    )
     callback_mock = Mock()
-    Pinger(targets=['8.8.8.8'], count=1, timeout=1,cb=callback_mock, start_running=True)
+    Pinger(
+        targets=["8.8.8.8"], count=1, timeout=1, cb=callback_mock, start_running=True
+    )
     await asyncio.sleep(0.1)  # Allow the Pinger to initialize
-    callback_mock.assert_called_with(*callback_response)  # Check if the callback was called at least once
+    callback_mock.assert_called_with(
+        *callback_response
+    )  # Check if the callback was called at least once
