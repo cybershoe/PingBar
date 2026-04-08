074c49159c3958f60fc66e64b8ac732ccb701e6d
Adam Schumacher <adam@genericor.com>
2026-04-08 16:25:29 -0400

    SettingsModel should provide a default list of IPs instead of an empty list Fixes #45

--- tests/pingrthingr/settings/test_settings.py
+++ tests/pingrthingr/settings/test_settings.py
@@ -6,38 +6,43 @@ from pingrthingr.settings import SettingsManager
 
 base_path = Path(__file__).parent
 
+@pytest.fixture
+def default_settings():
+    settings_file = base_path / "resources/default_settings.json"
+    return json_load(open(settings_file))
+
 class TestSettingsLoadAndSave:
-    def test_default_data(self):
+    def test_default_data(self, default_settings):
         # Test default loading of data
         settings_manager = SettingsManager("./nofile.json")
-        assert settings_manager._settings.model_dump() == {'display_mode': 'Dot', 'paused': False, 'targets': []}
+        assert settings_manager._settings.model_dump() == default_settings
 
-    def test_file_not_found(self, tmp_path):
+    def test_file_not_found(self, tmp_path, default_settings):
         # Test defaults loaded when file is not found
         settings_file = tmp_path / "badfile.json"
         settings_manager = SettingsManager(str(settings_file))
-        assert settings_manager._settings.model_dump() == {'display_mode': 'Dot', 'paused': False, 'targets': []}
+        assert settings_manager._settings.model_dump() == default_settings
 
-    def test_unreadable_file(self):
+    def test_unreadable_file(self, default_settings):
         # Test defaults loaded when file is unreadable
         with patch("pingrthingr.settings.settings.open", side_effect=PermissionError("Permission denied")):
             settings_manager = SettingsManager(str("unreadable.json"))
-        assert settings_manager._settings.model_dump() == {'display_mode': 'Dot', 'paused': False, 'targets': []}
+        assert settings_manager._settings.model_dump() == default_settings
 
-    def test_malformed_file(self, tmp_path):
+    def test_malformed_file(self, tmp_path, default_settings):
         # Test defaults loaded when file is malformed
         settings_file = tmp_path / "badfile.json"
         settings_file.write_text("not a json file")
         settings_manager = SettingsManager(str(settings_file))
-        assert settings_manager._settings.model_dump() == {'display_mode': 'Dot', 'paused': False, 'targets': []} 
+        assert settings_manager._settings.model_dump() == default_settings
 
     @pytest.mark.parametrize("settings_json", json_load(open(base_path / "resources/invalid_settings.json")))
-    def test_invalid_data(self, tmp_path, settings_json):
+    def test_invalid_data(self, tmp_path, settings_json, default_settings):
         # Test defaults loaded when file contains invalid data
         settings_file = tmp_path / "badfile.json"
         settings_file.write_text(json_dumps(settings_json))
         settings_manager = SettingsManager(str(settings_file))
-        assert settings_manager._settings.model_dump() == {'display_mode': 'Dot', 'paused': False, 'targets': []} 
+        assert settings_manager._settings.model_dump() == default_settings
 
     def test_valid_data(self):
         # Test loading of valid data
